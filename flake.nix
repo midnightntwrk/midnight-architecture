f524a89997c7520660ff8a1ec2ad00e8624b834a
Sose Magarian <sose.magarian@iohk.io>
2022-11-22 15:45:29 -0300

    Add tullia in flake.nix + run treefmt

--- flake.nix
+++ flake.nix
@@ -11,12 +11,9 @@
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    cicero = {
-      url = "github:input-output-hk/cicero";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        utils.follows = "utils";
-      };
+    tullia = {
+      url = "github:input-output-hk/tullia?rev=f75ad427298eb8adf1a14cf08a5c48d3d3aa8ca1";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -25,7 +22,7 @@
     nixpkgs,
     utils,
     devshell,
-    cicero,
+    tullia,
     ...
   }:
     utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"]
@@ -38,67 +35,62 @@
           hash = "sha256-6ad6CUz1UAvNkhdUJhOME7OsLpIXiBoERfTmowzTz64=";
         };
       });
-    in rec {
-      packages.midnight-architecture = pkgs.stdenv.mkDerivation {
-        name = "midnight-architecture";
-        src = ./.;
-        buildInputs = [
-          plantuml-pdf
-        ];
-        installPhase = ''
-          make -p \
-          | grep '^default:' \
-          | cut -d ' ' -f 2- --output-delimiter $'\n' \
-          | while read -r; do
-            mkdir -p $out/"$(dirname "$REPLY")"
-            mv "$REPLY" $out/"$REPLY"
-          done
-        '';
-      };
+    in
+      rec {
+        packages.midnight-architecture = pkgs.stdenv.mkDerivation {
+          name = "midnight-architecture";
+          src = ./.;
+          buildInputs = [
+            plantuml-pdf
+          ];
+          installPhase = ''
+            make -p \
+            | grep '^default:' \
+            | cut -d ' ' -f 2- --output-delimiter $'\n' \
+            | while read -r; do
+              mkdir -p $out/"$(dirname "$REPLY")"
+              mv "$REPLY" $out/"$REPLY"
+            done
+          '';
+        };
 
-      defaultPackage = packages.midnight-architecture;
+        defaultPackage = packages.midnight-architecture;
 
-      devShell = devshell.legacyPackages.${system}.mkShell {
-        # graphviz and setting GRAPHVIZ_DOT environment variables are needed for editors integration, though it doesn't work quite well
-        packages = [pkgs.graphviz pkgs.jdk];
-        env = [
-          {
-            name = "GRAPHVIZ_DOT";
-            value = pkgs.graphviz + "/bin/dot";
-          }
-          {
-            name = "JAVA_HOME";
-            value = pkgs.jdk;
-          }
-        ];
-        commands = [
-          {
-            package = "adrgen";
-            category = "ADR";
-          }
-          {
-            package = "treefmt";
-            category = "formatter";
-          }
-          {
-            package = pkgs.alejandra;
-            category = "formatter";
-          }
-          {
-            package = plantuml-pdf;
-            category = "diagram generator";
-          }
-        ];
-      };
-    })
-    // {
-      ciceroActions =
-        cicero.lib.callActionsWithExtraArgs
-        rec {
-          inherit (cicero.lib) std;
-          inherit (nixpkgs) lib;
-          actionLib = import "${cicero}/action-lib.nix" {inherit std lib;};
-        }
-        ./cicero;
-    };
+        devShell = devshell.legacyPackages.${system}.mkShell {
+          # graphviz and setting GRAPHVIZ_DOT environment variables are needed for editors integration, though it doesn't work quite well
+          packages = [pkgs.graphviz pkgs.jdk];
+          env = [
+            {
+              name = "GRAPHVIZ_DOT";
+              value = pkgs.graphviz + "/bin/dot";
+            }
+            {
+              name = "JAVA_HOME";
+              value = pkgs.jdk;
+            }
+          ];
+          commands = [
+            {
+              package = "adrgen";
+              category = "ADR";
+            }
+            {
+              package = "treefmt";
+              category = "formatter";
+            }
+            {
+              package = pkgs.alejandra;
+              category = "formatter";
+            }
+            {
+              package = plantuml-pdf;
+              category = "diagram generator";
+            }
+          ];
+        };
+      }
+      // tullia.fromSimple system {
+        tasks = import tullia/tasks.nix;
+        actions = import tullia/actions.nix;
+      });
 }
